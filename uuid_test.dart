fcd4f308543617a3e6aae4bf46c5d84286aca0c7
vvakame <vvakame+dev@gmail.com>
2012-05-12 17:18:47 +0900

    見た目の調整とか

--- uuid_test.dart
+++ uuid_test.dart
@@ -4,36 +4,46 @@
 
 void main(){
   RegExp uuidPattern = new RegExp('^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\$');
+  
   describe("test pattern sanity", () {
     it("matches UUID syntax", () {
       expect(uuidPattern.hasMatch('f81d4fae-7dec-11d0-a765-00a0c91e6bf6')).toBe(true);
     });
   });
-  describe("UUID v0", () {
+  
+  describe("UUID test", () {
+
     it("suits UUID syntax", () {
       UUID uuid = new UUID_v0();
       expect(uuidPattern.hasMatch(uuid.generate())).toBe(true);
     });
-  });
-  describe("UUID v1", () {
-    it("suits UUID syntax", () {
+
+    it("v1 suits UUID syntax", () {
       UUID uuid = new UUID_v1();
       expect(uuidPattern.hasMatch(uuid.generate())).toBe(true);
     });
-  });
-  describe("UUID v4", () {
-    it("suits UUID syntax", () {
+
+    it("v4 suits UUID syntax", () {
       UUID uuid = new UUID_v4();
       expect(uuidPattern.hasMatch(uuid.generate())).toBe(true);
     });
-  });
-  describe("UUID", () {
+
     it("suits UUID syntax", () {
       UUID uuid = new UUID();
       expect(uuidPattern.hasMatch(uuid.generate())).toBe(true);
     });
   });
+  
+  describe("sample fail.", (){
+    it("success", (){
+      expect(1).toBe(1);
+    });
+    
+    it("failure...", (){
+      expect(1).toBe(2);
+    });
+  });
 
-  setTeaoliveReporter(new TeaoliceHtmlReporter());
+  setTeaoliveReporter(new TeaoliveHtmlReporter());
   teaoliveRun();
 }
